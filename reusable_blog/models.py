7cba4f15b37d4db75885cbae88314b0630868296
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2017-10-04 08:10:20 +0100

    Update to add 'settings' import to models.py

--- reusable_blog/models.py
+++ reusable_blog/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 from django.utils import timezone
 
+from django.conf import settings
+
 # Create your models here.
 class Post(models.Model):
 	##Here we'll define our post model
